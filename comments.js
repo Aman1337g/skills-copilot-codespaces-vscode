58a7a4aa6d739c66ffffac0c28c32f41ad279de0
Aman Kumar Gupta <97891757+Aman1337g@users.noreply.github.com>
2023-07-21 11:45:38 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,69 +1,61 @@
-// create web server
-// get comments from db
-// send comments to client
+// Create web server
+// Run: node comments.js
+// Test: curl -i http://localhost:3000/comments
+// Test: curl -i http://localhost:3000/comments/1
+// Test: curl -i -X POST -H "Content-Type: application/json" -d '{"body":"This is a comment","postId":1}' http://localhost:3000/comments
+// Test: curl -i -X PUT -H "Content-Type: application/json" -d '{"body":"This is a comment","postId":1}' http://localhost:3000/comments/1
+// Test: curl -i -X DELETE http://localhost:3000/comments/1
 
-// import modules
 var express = require('express');
-var router = express.Router();
-var fs = require('fs');
-var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
 
-// import my modules
-var db = require('../db');
+var comments = [
+  { id: 1, postId: 1, body: 'This is a comment' },
+  { id: 2, postId: 1, body: 'This is another comment' },
+  { id: 3, postId: 2, body: 'This is yet another comment' }
+];
 
-// route: /comments
-router.get('/', function(req, res, next) {
-  // get comments from db
-  db.getComments(function(err, comments) {
-    if (err) {
-      return next(err);
-    }
-    // send comments to client
-    res.json(comments);
-  });
+// GET /comments
+app.get('/comments', function(req, res) {
+  res.json(comments);
 });
 
-// route: /comments
-router.post('/', function(req, res, next) {
-  // get comment from client
-  var comment = req.body;
-  // add comment to db
-  db.addComment(comment, function(err, comment) {
-    if (err) {
-      return next(err);
-    }
-    // send comment to client
+// GET /comments/:id
+app.get('/comments/:id', function(req, res) {
+  var comment = comments.find(function(comment) {
+    return comment.id === parseInt(req.params.id);
+  });
+  if (comment) {
     res.json(comment);
-  });
+  } else {
+    res.status(404).json({ error: 'Comment not found' });
+  }
 });
 
-// route: /comments/:id
-router.put('/:id', function(req, res, next) {
-  // get comment from client
-  var comment = req.body;
-  // update comment in db
-  db.updateComment(comment, function(err, comment) {
-    if (err) {
-      return next(err);
-    }
-    // send comment to client
+// POST /comments
+app.post('/comments', function(req, res) {
+  var maxId = comments.reduce(function(max, comment) {
+    return comment.id > max ? comment.id : max;
+  }, 0);
+  var newComment = {
+    id: maxId + 1,
+    postId: req.body.postId,
+    body: req.body.body
+  };
+  comments.push(newComment);
+  res.json(newComment);
+});
+
+// PUT /comments/:id
+app.put('/comments/:id', function(req, res) {
+  var comment = comments.find(function(comment) {
+    return comment.id === parseInt(req.params.id);
+  });
+  if (comment) {
+    comment.postId = req.body.postId;
+    comment.body = req.body.body;
     res.json(comment);
-  });
-});
-
-// route: /comments/:id
-router.delete('/:id', function(req, res, next) {
-  // get comment id from client
-  var id = req.params.id;
-  // delete comment from db
-  db.deleteComment(id, function(err, comment) {
-    if (err) {
-      return next(err);
-    }
-    // send comment to client
-    res.json(comment);
-  });
-});
-
-// export router
-module.exports = router;
+  } else {
+    res
